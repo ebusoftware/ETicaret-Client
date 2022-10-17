0efd8348d76226af250e1245c169eccd00e33a5a
ebusoftware <e3b8u@hotmail.com>
2022-10-17 21:50:08 +0300

    User Identity

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './admin/components/dashboard/dashboard.component';
 import { LayoutComponent } from './admin/layout/layout.component';
 import { HomeComponent } from './ui/components/home/home.component';
-
 const routes: Routes = [
   {
     path: "admin", component: LayoutComponent, children: [
@@ -16,6 +15,7 @@ const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "basket", loadChildren: () => import("./ui/components/baskets/baskets.module").then(module => module.BasketsModule) },
   { path: "products", loadChildren: () => import("./ui/components/products/products.module").then(module => module.ProductsModule) },
+  { path: "register", loadChildren: () => import("./ui/components/register/register.module").then(module => module.RegisterModule) },
 ];
 
 @NgModule({
--- src/app/ui/components/components.module.ts
+++ src/app/ui/components/components.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductsModule } from './products/products.module';
-import { BasketsModule } from './baskets/baskets.module';
 import { HomeModule } from './home/home.module';
-
+import { BasketsModule } from './baskets/baskets.module';
+import { RegisterComponent } from './register/register.component';
+import { RegisterModule } from './register/register.module';
 
 
 @NgModule({
   declarations: [
+
   ],
   imports: [
     CommonModule,
     ProductsModule,
+    HomeModule,
     BasketsModule,
-    HomeModule
+    RegisterModule
   ]
 })
 export class ComponentsModule { }
